5ebd418f0795a9a6cf6fabbb970fa7e8207eb0c3
kindamu24005 <107189394+kindamu24005@users.noreply.github.com>
2022-06-22 23:40:18 +0900

    finish section 3

--- test.py
+++ test.py
@@ -1,6 +1,46 @@
-print("test")
+import math
 
-a=10
-b=a
-c=b
-print(c)
+result = math.gcd(1,4)
+print(result)
+
+print("hello. \n How are you")
+print("""
+line1
+line2
+line3
+""")
+
+print("Hi." * 3)
+s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa""bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+print(s)
+
+
+
+word = "python"
+print(word[0:2])
+print(word[:2])
+print(word[2:])
+word = "j" + word[1:]
+print(word)
+
+s = "My name is Mike. Hi Mike."
+print(s)
+is_start = s.startswith("My")
+print(is_start)
+is_start = s.startswith("X")
+print(is_start)
+print(s.find("Mike"))
+print(s.rfind("Mike"))
+print(s.count("Mike"))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+
+name = "Toshi"
+family = "O"
+print(f"My name is {name}. Watashi ha {family}")
+
+
+
+
